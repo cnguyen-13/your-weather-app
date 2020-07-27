952ec1afee806ae99ebdcb9d58d369e6b0297786
cnguyen-13 <kpnguyen1013@gmail.com>
2020-07-26 23:07:30 -0500

    API for background image complete

--- src/Components/InfoPage/InfoHeader.tsx
+++ src/Components/InfoPage/InfoHeader.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+const apiKeyUnsplash = "jY1QfhlpqNPs4wI8AywokF-l_LpooDq4HRauIfn7HZI";
 interface Props {
     city: string | null;
     country: string | null;
@@ -7,12 +7,49 @@ interface Props {
 
 function InfoHeader(props: Props) {
     const { city, country } = props;
+    const [bgImage, setBgImage] = useState<string | null>(null);
+    useEffect(() => {
+        async function getBgImage() {
+            const res = await fetch(
+                `https://api.unsplash.com/search/photos?query=${city}&orientation=landscape&client_id=${apiKeyUnsplash}`
+            );
+            const data = await res.json();
+            const arr = data.results;
+            const firstImg = arr[0];
+            const image = firstImg.urls.full;
+            console.log(image);
+            setBgImage(image);
+        }
 
-    return (
-        <h2 className="section-title">
-            City of {city} in {country}
-        </h2>
-    );
+        getBgImage();
+    }, [city]);
+
+    if (bgImage) {
+        return (
+            <section
+                className="city-bg mask"
+                style={{
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${bgImage})`,
+                }}
+            >
+                <h2 className="city-title">
+                    City of {city}
+                    <span className="city-subtitle">, {country}</span>
+                </h2>
+            </section>
+        );
+    } else {
+        return (
+            <section className="city-bg">
+                <h2 className="city-title">
+                    Loading...
+                    <span className="city-subtitle"></span>
+                </h2>
+            </section>
+        );
+    }
 }
 
 export default InfoHeader;
